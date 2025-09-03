81cdff8ffec35b065521f45b6ff4714184478640
davi3234 <davi.fadriano@gmail.com>
2025-09-03 09:40:41 -0300

    test: Criando teste unitário para pessoa somente com testes funcionais

---
+++ src/Test/PessoaTeste.php
@@ -0,0 +1,116 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Util\Test;
+
+class PessoaTeste extends TestCase{
+
+  private IPessoaRepositorio $pessoaRepositorio; 
+  private PessoaController $pessoaController;
+
+  protected function setUp(): void{
+    parent::setUp();
+    $this->pessoaRepositorio = $this->createMock(IPessoaRepositorio::class);
+    $this->pessoaController = new PessoaController($this->pessoaRepositorio);
+  }
+
+  #[Test]
+  public function testaCriarPessoaComDadosFuncionais(){
+    //Arrange
+    $id = 1;
+    $nome = "Davi";
+    $cpf = "785.609.419-24";
+
+    $pessoa = new Pessoa(
+      nome: $nome, 
+      cpf: $cpf
+    );
+    $pessoaDepois = new Pessoa(
+      id: $id,
+      nome: $nome,
+      cpf: $cpf
+    );
+
+    $this->pessoaRepositorio
+      ->method('criar')
+        ->with($pessoa)
+          ->willReturn($pessoaDepois);
+
+    //Act
+    $resultado = $this->pessoaController->criarPessoa(
+      [
+        'nome' => $nome,
+        'cpf' => $cpf
+      ]
+    );
+
+    $resultadoEsperado = [
+      'success' => true,
+      'status' => 201,
+      'message' => 'Pessoa criada com Sucesso',
+      'data' => [
+        'pessoa' => [
+          'nome' => $nome,
+          'cpf' => $cpf
+        ]
+      ]
+    ];
+
+    //Assert
+    $this->assertEquals($resultado, $resultadoEsperado);
+  }
+
+  #[Test]
+  public function testaCriarPessoaSemCpf(){
+    $this->expectException(Exception::class);
+
+    //Arrange
+    $nome = "Davi";
+    $cpf = "";
+
+    $pessoa = new Pessoa(
+      nome: $nome,
+      cpf: $cpf
+    );
+
+    $this->pessoaRepositorio
+      ->method('criar')
+        ->with($pessoa)
+          ->willThrowException();
+
+    //Act
+    $resultado = $this->pessoaController->criarPessoa(
+      [
+        'nome' => $nome,
+        'cpf' => $cpf
+      ]
+    );
+  }
+
+  #[Test]
+  public function testaCriarPessoaSemNome(){
+    $this->expectException(Exception::class);
+
+    //Arrange
+    $nome = "";
+    $cpf = "785.609.419-24";
+
+    $pessoa = new Pessoa(
+      nome: $nome,
+      cpf: $cpf
+    );
+
+    $this->pessoaRepositorio
+      ->method('criar')
+        ->with($pessoa)
+          ->willThrowException();
+
+    //Act
+    $resultado = $this->pessoaController->criarPessoa(
+      [
+        'nome' => $nome,
+        'cpf' => $cpf
+      ]
+    );
+  }
+}
