6c2853a5c9caf5dbdf3092c7b64e84f0982be01f
davi3234 <davi.fadriano@gmail.com>
2025-09-03 11:38:06 -0300

    refactor: Reestruturando forma de receber requisição para não usar mais a classe server

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
-use Core\Server\Server;
+use Core\Exception\HttpException;
+use Core\Server\Request;
+use Core\Server\Response;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
@@ -9,11 +12,70 @@ require_once __DIR__ . '/../bootstrap.php';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
+  http_response_code(200);
+  exit;
 }
 
-$server = Server::Bootstrap();
-$server->dispatch();
+$response = new Response();
+
+try {
+  $routes = require __DIR__ . '/routes.php';
+
+  $request = Request::fromGlobals();
+  $metodoHttp = $request->getMethod();
+  $uri = $request->getRouter();
+  $params = $request->getParams();
+
+  if (!isset($routes[$metodoHttp])) {
+    throw new HttpException(405, ['Rota não suportada para este método']);
+  }
+
+  $found = null;
+  $routeParams = [];
+
+  foreach ($routes[$metodoHttp] as $route => $config) {
+    $pattern = preg_replace('#\{[a-zA-Z_]+\}#', '([a-zA-Z0-9_-]+)', $route);
+    $pattern = "#^" . $pattern . "$#";
+
+    if (preg_match($pattern, $uri, $matches)) {
+      array_shift($matches);
+
+      preg_match_all('#\{([a-zA-Z_]+)\}#', $route, $paramNames);
+      $routeParams = array_combine($paramNames[1], $matches);
+
+      $found = $config;
+      break;
+    }
+  }
+
+  if (!$found) {
+    throw new HttpException(404, ['Rota não encontrada']);
+  }
+
+  $controller = new $found['controller']();
+  $action = $found['action'];
+
+  if (!method_exists($controller, $action)) {
+    throw new HttpException(500, ["Método {$action} não encontrado"]);
+  }
+
+  $allParams = array_merge($params, $routeParams);
+
+  $result = $controller->$action($allParams);
+
+  $response->setSuccess($result);
+
+  echo $response->toJson();
+
+} 
+catch (HttpException $e) {
+  $response->setError($e->getErrors(), $e->getCode());
+
+  echo $response->toJson();
+}
+catch(Exception $e){
+  $response->setError(['Ocorreu algum erro Interno', 500]);
+}
