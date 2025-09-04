91dc142e1daf3ff31dad4cda82492e687109f1ba
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-09-03 22:55:22 -0300

    fix: Corrigido identação e utilizando commit de transação

--- src/Model/PessoaRepositorio.php
+++ src/Model/PessoaRepositorio.php
@@ -5,50 +5,53 @@ namespace Model;
 use Core\EntityManagerSingleton;
 use Doctrine\ORM\EntityManager;
 use Exception;
+
 class PessoaRepositorio implements IPessoaRepositorio{
-  private EntityManager $entityManager;
+    private EntityManager $entityManager;
 
-  public function __construct(){
-    $this->entityManager = EntityManagerSingleton::getInstance();
-  }
+    public function __construct(){
+        $this->entityManager = EntityManagerSingleton::getInstance();
+    }
 
-  public function criar(Pessoa $pessoa): Pessoa{
-    try{
-      $this->entityManager->beginTransaction();
-      $this->entityManager->persist($pessoa);
-      $this->entityManager->flush();
-     
-      return $pessoa;
-    }
-    catch (Exception $e){
-      $this->entityManager->rollback();
-      throw $e;
-    }
-  }
-  public function editar(Pessoa $pessoa): Pessoa{
-    try{
-      $this->entityManager->beginTransaction();
-      $this->entityManager->persist($pessoa);
-      $this->entityManager->flush();
-     
-      return $pessoa;
-    }
-    catch (Exception $e){
-      $this->entityManager->rollback();
-      throw $e;
-    }
-  }
-  public function excluir(int $id): void{
-    try{
-      $this->entityManager->beginTransaction();
+    public function criar(Pessoa $pessoa): Pessoa{
+        try {
+            $this->entityManager->beginTransaction();
+            $this->entityManager->persist($pessoa);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
 
-      $pessoa = $this->entityManager->find(Pessoa::class, $id);
+            return $pessoa;
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
+    }
 
-      $this->entityManager->remove($pessoa);
+    public function editar(Pessoa $pessoa): Pessoa{
+        try {
+            $this->entityManager->beginTransaction();
+            $this->entityManager->persist($pessoa);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            return $pessoa;
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
-    catch (Exception $e){
-      $this->entityManager->rollback();
-      throw $e;
+
+    public function excluir(int $id): void{
+        try {
+            $this->entityManager->beginTransaction();
+
+            $pessoa = $this->entityManager->find(Pessoa::class, $id);
+
+            $this->entityManager->remove($pessoa);
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
-  }
 }
