0a8d64aa05460286943183efe3516766e1b861bd
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-09-03 23:32:15 -0300

    feat: Mapeado endpoints para receber as requisições de pessoa

--- public/routes.php
+++ public/routes.php
@@ -1,9 +1,31 @@
 <?php
 return [
-  'POST' => [
-    '/pessoas' => [
-      'controller' => \Controller\PessoaController::class,
-      'action' => 'criarPessoa'
+    'POST' => [
+        '/pessoas' => [
+            'controller' => \Controller\PessoaController::class,
+            'action' => 'criarPessoa'
+        ]
+    ],
+    'GET' => [
+        '/pessoas' => [
+            'controller' => \Controller\PessoaController::class,
+            'action' => 'listarPessoas'
+        ],
+        '/pessoas/{id}' => [
+            'controller' => \Controller\PessoaController::class,
+            'action' => 'buscarPessoa'
+        ]
+    ],
+    'PUT' => [
+        '/pessoas/{id}' => [
+            'controller' => \Controller\PessoaController::class,
+            'action' => 'editarPessoa'
+        ]
+    ],
+    'DELETE' => [
+        '/pessoas/{id}' => [
+            'controller' => \Controller\PessoaController::class,
+            'action' => 'excluirPessoa'
+        ]
     ]
-  ]
 ];
