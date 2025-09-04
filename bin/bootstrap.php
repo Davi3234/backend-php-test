c1cabd4e2ae54f0811ebe6cbc82a36936ebcd4d1
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-09-03 22:47:06 -0300

    config: Trocando configurações do doctrine e variáveis de ambiente para pasta bin

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -2,11 +2,9 @@
 
 use Core\EntityManagerSingleton;
 use Dotenv\Dotenv;
-require_once "vendor/autoload.php";
+require_once __DIR__."/../vendor/autoload.php";
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
 $dotenv->load();
-
-$isDevMode = true;
 
 $entityManager = EntityManagerSingleton::getInstance();
